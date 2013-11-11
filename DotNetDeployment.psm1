3c8cb94478a09e87ac061da10ce15d2654d33f00
Mike Brooks <mike.brooks@centricconsulting.com>
2013-11-11 09:50:29 -0500

    Sandbox stuff

---
+++ DotNetDeployment.psm1
@@ -0,0 +1,261 @@
+﻿# Interrogates Package file for evidence of NServiceBus.
+# Returns $true if the pachage contains NServiceBus libraries, $false otherwise
+function IsNServiceBusService {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the MsDeploy Package file.")]
+		[System.IO.FileInfo]$packageFile		
+	)
+	[string[]]$msdeployArgs = @(
+	  "-verb:dump",
+	  "-xml",
+	  "-source:package='$packageFile'")
+	  
+	$xml = InvokeMsDeploy $msdeployArgs	
+	
+	if ($lastexitcode -ne 0) {
+		throw ("Error inspecting $packageFile!")
+	}
+	
+	$node = ([Xml]$xml).SelectSingleNode("//filePath[@path='NServiceBus.Host.exe']")
+	return ($node -ne $null)
+}
+
+# Query KeePass using KPScript
+# KeePass manages Name Value Pairs by Group, Title, and Field.  Group and Title are user defined.  Field is KeePass
+# defined (e.g. UserName, Password, etc.)
+function QueryKeePass {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the group containing the Title to Query for")]
+		[String]$groupName,
+		[parameter(Mandatory=$true, Position=1, HelpMessage="The name of the Title to query for")]
+		[String]$title,
+		[parameter(Mandatory=$true, Position=2, HelpMessage="The name of the Field to query for")]
+		[String]$fieldName
+	)
+	
+	#TODO: Update paths with production locations.  Will also need to deal with how to securely access the keepass password
+	# Keypass parameters
+	$kpscriptPath = "C:\Program Files (x86)\KeePass Password Safe 2\kpscript.exe"
+	$keepassDataBase = "C:\Users\brooksm\Documents\TestDatabase.kdbx"
+	$keepassKeyFile = "C:\Users\brooksm\Documents\TestDatabase.key"
+	$keepassPassword = "Halle2003"
+	
+	[String[]]$kpsArgs = @(
+		"-c:GetEntryString",
+		"`"$keepassDataBase`"",
+		"-pw:`"$keepassPassword`"",
+		"-keyfile:`"$keepassKeyFile`"",
+		"-ref-Title:`"$title`"",
+		"-Field:$fieldName",
+		"-GroupName:`"$groupName`""
+		)
+	
+	$output = @(cmd.exe /C $("`"`"$kpscriptPath`" $kpsArgs`""))
+	if ($lastexitcode -ne 0) {
+		$output
+		throw ("Error executing KPScript!")
+	}
+	
+	if ($output[$output.Length - 1] -eq "OK: Operation completed successfully.") {
+		#copy all elements except the last to the new array
+		[String[]]$newArray = @()
+		for($i=0; $i -lt $output.Length - 1; $i++) {
+			$newArray += $output[$i]
+		}
+		$newArray
+	}
+	else {
+		throw ("Error occurred while querying KeePass - $output")
+	}
+}
+
+function InvokeMsDeploy {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="MsDeploy parameters defined as a string array.")]
+		[String[]]$parameters
+	)
+	$msdeploy_path = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+	$output = cmd.exe /C $("`"`"$msdeploy_path`" $parameters`"")
+	$output
+}
+
+function GetInstallationPath {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The MsDeploy 'DestManifext.xml' file.")]
+		[System.IO.FileInfo]$destManifest
+	)
+	
+	[xml]$xml = Get-Content $destManifest
+	$path = $xml.sitemanifest.contentPath.path
+	$path
+}
+
+# Function to deploy a regular Windows Service
+# This functions does the following:
+# 1. Stops and Removes the service if it already exists
+# 2. Deploys the files via WebDeploy
+# 3. Reinstalls and restarts the service
+function DeployDotNetWindowsService {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the MsDeploy Package folder to be deployed.")]
+		[System.IO.FileInfo]$packageFile,
+		[parameter(Mandatory=$true, Position=1, HelpMessage="The name of the computer to install the service on.")]
+		[string]$computerName,
+		[parameter(Mandatory=$true, Position=2, HelpMessage="The name of the windows service")]
+		[string]$serviceName,
+		[parameter(Mandatory=$true, Position=3, HelpMessage="The username to create the service under")]
+		[AllowEmptyString()]
+		[string]$userName,
+		[parameter(Mandatory=$true, Position=4, HelpMessage="The password of the username")]
+		[AllowEmptyString()]
+		[string]$password
+	)
+
+	# Stop the service
+	StopService $computerName $serviceName $true
+
+	# Deploy the new service files
+	$fileNoExt = [System.IO.Path]::GetFileNameWithoutExtension($packageFile)	
+	$destManifestFile = "$($packageFile.DirectoryName)\$($fileNoExt).DestManifest.xml"
+	$settingsFile = "$($packageFile.DirectoryName)\$($fileNoExt).SetParameters.xml"
+	$destinationRoot = GetInstallationPath $destManifestFile
+	$destinationPath = "$($destinationRoot)\bin\$($fileNoExt).exe"
+
+	[string[]]$msdeployArgs = @(
+	  "-verb:sync",
+	  "-source:package='$packageFile'",
+	  "-dest:manifest=`"$destManifestFile`",computerName=`"$computerName`",authtype=`"NTLM`",includeAcls=`"False`"",
+	  "-disableLink:AppPoolExtension",
+	  "-disableLink:ContentExtension",
+	  "-disableLink:CertificateExtension"
+	  "-setParamFile:`"$settingsFile`"" 
+	  "-enableRule:DoNotDeleteRule"
+	  "-allowUntrusted"
+	  "-verbose"
+	  )
+	  
+	InvokeMsDeploy $msdeployArgs
+	if ($lastexitcode -ne 0) {
+		throw ("Error deploying $($packageFile)!")
+	}
+
+	# Create the service
+	$Wmi = [wmiclass]("\\$computerName\ROOT\CIMV2:Win32_Service")
+	if ($Wmi -eq $null) {
+		throw "Error accessing 'Root\Cimv2' Namespace on $($computerName)"
+	}	
+
+	$inparams = $Wmi.PSBase.GetMethodParameters("Create")
+	$inparams.DesktopInteract = $false
+	$inparams.DisplayName = $serviceName
+	$inparams.ErrorControl = 0
+	$inparams.LoadOrderGroup = $null
+	$inparams.LoadOrderGroupDependencies = $null
+	$inparams.Name = $serviceName
+	$inparams.PathName = $destinationPath
+	$inparams.ServiceDependencies = $null
+	$inparams.ServiceType = 16
+	$inparams.StartMode = "Manual"
+	$inparams.StartName = if ([string]::IsNullOrEmpty($userName) -eq $true) { $null } else { $userName }
+	$inparams.StartPassword = if ([string]::IsNullOrEmpty($password) -eq $true) { $null } else { $password }
+	
+	$result = $Wmi.PSBase.InvokeMethod("Create", $inparams, $null)
+	if ($result.ReturnValue -ne 0) {
+		throw ("Unable to create windows service on $($computerName)! ReturnValue = $($result.ReturnValue)")
+	}
+	
+	# Start the service
+	#TODO:  Uncomment out once credential management issues have been resolved
+	#StartService $computerName $serviceName
+}
+
+function DeployNServiceBusWindowsService {
+	#TODO: Implement Function
+}
+
+function DeployDotNetWebApplication {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the MsDeploy Package file to be deployed.")]
+		[System.IO.FileInfo]$packageFile,
+		[parameter(Mandatory=$true, Position=1, HelpMessage="The name of the MsDeploy Settings file to be used at deployment.")]
+		[System.IO.FileInfo]$settingsFile,
+		[parameter(Mandatory=$true, Position=2, HelpMessage="The name of the server to install the web application.")]
+		[string]$serverName,
+		[parameter(Mandatory=$true, Position=3, HelpMessage="The web site name to deploy to")]
+		[string]$siteName,
+		[parameter(Mandatory=$true, Position=4, HelpMessage="The virtual path to deploy to")]
+		[string]$applicationPath,
+		[parameter(Mandatory=$true, Position=5, HelpMessage="The username to create the service under")]
+		[AllowEmptyString()]
+		[string]$userName,
+		[parameter(Mandatory=$true, Position=6, HelpMessage="The password of the username")]
+		[AllowEmptyString()]
+		[string]$password
+		)
+
+		[string[]]$msdeployArgs = @(
+		  "-verb:sync",
+		  "-source:package='$packageFile'",
+		  "-dest:auto,computerName=`"https://$($serverName):8172/msdeploy.axd?site=$($siteName)`",authtype=`"Basic`",username=`"$($userName)`",password=`"$($password)`",includeAcls=`"True`"",
+		  "-retryAttempts:5",
+		  "-retryInterval:3000",
+		  "-setParam:name=`"IIS Web Application Name`",value=`"$($applicationPath)`"", 
+		  "-enableLink:AppPoolExtension",
+		  "-disableLink:ContentExtension",
+		  "-disableLink:CertificateExtension"
+		  "-setParamFile:`"$settingsFile`"" 
+		  "-enableRule:DoNotDeleteRule"
+		  "-allowUntrusted"
+		  "-verbose"
+		)
+		
+		InvokeMsDeploy $msdeployArgs
+		if ($lastexitcode -ne 0) {
+			throw ("Error deploying $($packageFolder.Name)!")
+		}
+}
+
+# Generic function to stop a service and optionally uninstall
+function StopService {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the computer that the service is on.")]
+		[string]$computerName,
+		[parameter(Mandatory=$true, Position=1, HelpMessage="The name of the windows service")]
+		[string]$serviceName,
+		[parameter(Position=2, HelpMessage="The name of the computer that the service is on.")]
+		[Boolean]$remove = $false
+	)
+	
+	# Stop the service (if running) and delete it
+	$service = Get-WmiObject -Class Win32_Service -ComputerName $computerName -Filter "Name = '$serviceName'"
+	if ($service -ne $null) 
+	{ 
+		if ($service.State -eq "Running") { $service.StopService() }
+	    if ($remove -eq $true) { $service.Delete() | out-null }
+	}
+}
+
+# Generic function to start a service
+function StartService {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the computer that the service is on.")]
+		[string]$computerName,
+		[parameter(Mandatory=$true, Position=1, HelpMessage="The name of the windows service")]
+		[string]$serviceName
+	)
+	
+	# Start the service
+	$service = Get-WmiObject -Class Win32_Service -ComputerName $computerName -Filter "Name = '$serviceName'"
+	if ($service -ne $null) { 
+		$startResult = $service.StartService()
+		if ($startResult.ReturnValue -ne 0) {
+			#TODO:  Uncomment throw once credential management issues are resolved
+			#throw ("Unable to start service $($serviceName)! ReturnValue = $($startResult.ReturnValue)")
+			"Unable to start service $($serviceName)! ReturnValue = $($startResult.ReturnValue)"
+		}
+		"Service $($serviceName) successfully started on $($computerName)!"
+	}
+	else {
+		throw "Unable to start $($serviceName)!"
+	}
+}
---
+++ psake-build.ps1
@@ -0,0 +1,296 @@
+﻿# Load dependent modules
+if (!(Get-Module ".\DotNetDeployment.psm1")) { 
+    Import-Module ".\DotNetDeployment.psm1" -ErrorAction Stop
+}
+
+Properties {
+	#Contants
+	$script_file_dir = Split-Path $psake.build_script_file
+	$build_artifacts_dir = "$build_dir\BuildArtifacts\"
+	$code_dir = $script_file_dir
+	$mstest_path = "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\MSTest.exe"
+	$msdeploy_path = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+	#MSBuild Variables
+	$ProjectsToBuild = "$code_dir\Safe Auto Systems.sln"
+	$Configuration = "Debug"
+	$Platform = "AnyCPU"
+	$OutDir = "$script_file_dir\BuildArtifacts\"
+	$deploymentConfig = "Deployment.config"
+	$Verbosity = "/v:Normal"
+	$MaxCpuCount = 1
+	$FileLogger = ""
+	$DistributedLogger = ""
+	$TFSServerUrl = ""
+	$BuildUri = ""
+	$TeamProject = ""
+	$DestManifest = ""
+}
+
+task default -depends CIBuild
+
+task CIBuild -depends Build, CIBuildUnitTest {
+}
+
+task DeploymentBuild -depends BuildAndPackage, DeploymentBuildUnitTest, DeployPackages {
+}
+
+task Clean {
+	Write-Output $ProjectsToBuild
+	Write-Output $Configuration
+	Write-Output $OutDir
+	Write-Output $Verbosity
+
+	if (Test-Path -Path $OutDir) {
+		Write-Output "Cleaning $($OutDir) recursively..."
+		$dir = Get-Item $OutDir
+		$dir.Delete($true)
+	}
+	
+	mkdir $OutDir | Out-Null
+	
+	Write-Output "Cleaning $ProjectsToBuild"
+	Exec { msbuild $ProjectsToBuild /t:Clean /p:Configuration=$Configuration /p:Platform=$Platform $Verbosity }
+}
+
+task Build {
+	# Parse Projects to Build
+	$projectFiles = ParseProjectsToBuild
+	# Fix the output dir variable if necessary
+	$OutDirPath = FixOutDirForMsBuild
+	foreach($projectFile in $projectFiles) {
+		Write-Output "Building $projectFile" -ForegroundColor Green
+		Exec { msbuild $projectFile /t:Build /p:Configuration=$Configuration /p:Platform=$Platform /p:OutDir=$OutDirPath /m:$MaxCpuCount /nr:false $FileLogger $DistributedLogger $Verbosity} 
+	}
+}
+
+task BuildAndPackage {
+	Write-Output $FileLogger
+	Write-Output $DistributedLogger
+	# Parse Projects to Build
+	$projectFiles = ParseProjectsToBuild
+	# Fix the output dir variable if necessary
+	$OutDirPath = FixOutDirForMsBuild
+	foreach($projectFile in $projectFiles) {
+		Write-Output "Building and Packaging $projectFile" -ForegroundColor Green
+		Exec { msbuild $projectFile /t:Build /p:Configuration=$Configuration /p:Platform=$Platform /p:OutDir=$OutDirPath /m:$MaxCpuCount /nr:false /p:DeployOnBuild=True /p:DeployTarget=Package /p:DestinationManifestRootPath=$DestManifest $FileLogger $DistributedLogger $Verbosity} 
+	}
+}
+
+task CIBuildUnitTest -depends Build {
+	RunUnitTests
+}
+#
+task DeploymentBuildUnitTest -depends BuildAndPackage {
+	RunUnitTests
+}
+
+task DeployPackages -depends BuildAndPackage {
+	Write-Output "Deploying Packages"
+	
+	$packages = Get-ChildItem "$OutDir\_PublishedWebsites\*_Package"
+	foreach($package in $packages) {
+		if (Test-Path "$package\*.DestManifest.xml") {
+			DeployWindowsServiceApplication $package	
+		}
+		else {		
+			DeployWebApplication $package
+		}
+	}
+}
+
+# Function to fix the OutDir variable if it contains spaces, it must end in "\\" due to a bug in MSBuild
+function FixOutDirForMsBuild {	
+	$tempPath = Resolve-Path $OutDir
+	if ($tempPath.Path.IndexOf(" ") -ge 0) {
+		$path = $tempPath.Path
+		if ($path.EndsWith("\")) {
+			$path = $path + "\"
+		}
+		else {
+			$path = $path + "\\"
+		}
+		return ($path)
+	}
+	return $tempPath.Path
+}
+
+function ParseProjectsToBuild {
+	return $ProjectsToBuild.split(";")
+}
+
+function RunUnitTests {
+	$assemblies = Get-ChildItem "$OutDir\*.Tests.dll"
+	if ($assemblies.Count -gt 0) {
+		Write-Output "Executing Unit Tests"
+
+		# Time Stamp will be added to our test files
+		$TimeStamp = (Get-Date -Format "yyyy-MM-dd.hh_mm_ss")
+		
+		# Add the test results file to the directory
+		$TestResultsFile = "`"$OutDir\${TimeStamp}.trx`""
+		$TestArgs = @()
+		foreach($assembly in $assemblies) {
+			$TestArgs += $("/testcontainer:`"$($assembly)`"")
+			Write-Output "Adding $assembly to tests"
+		}
+		$TestArgs += "/flavor:`"$Configuration`""
+		$TestArgs += "/platform:`"$Platform`""
+		$TestArgs += "/detail:errormessage"
+		$TestArgs += "/resultsfile:$TestResultsFile"
+		if ($TFSServerUrl -ne "") {
+			Write-Output "TFSServerUrl = $TFSServerUrl"
+			$TestArgs += "/publish:`"$TFSServerUrl`""
+		}
+		if ($BuildUri -ne "") {
+			Write-Output "BuildUri = $BuildUri"
+			$TestArgs += "/publishbuild:`"$BuildUri`""
+		}
+		if ($TeamProject -ne "") {
+			Write-Output "TeamProject = $TeamProject"
+			$TestArgs += "/teamproject:`"$TeamProject`""
+		}
+		
+		Write-Output $("`"$mstest_path`" $TestArgs")
+		#& $mstest_path $TestArgs /detail:errormessage /resultsfile:$TestResultsFile			
+		cmd.exe /C $("`"`"$mstest_path`" $TestArgs`"")
+		
+		if ($lastexitcode -ne 0) {
+			throw ("Error executing Tests!")
+		}
+		
+		$executed = 0
+		$passed = 0
+		$failed = 0
+		
+		if (Test-Path $TestResultsFile) {
+			$xml = [xml](Get-Content $TestResultsFile)
+			$counters = $xml.TestRun.ResultSummary.Counters
+			$executed += ($counters | Select total).total
+			$passed += ($counters | Select passed).passed
+			$failed += ($counters | Select failed).failed
+			$failed += ($counters | Select error).error
+			$failed += ($counters | Select inconclusive).inconclusive
+		}
+	
+		Write-Output "Tests Executed: $executed"
+		Write-Output "Tests Passed: $passed"
+		Write-Output "Tests Failed: $failed"
+		#TODO: Fail process if tests fail		
+	}
+}
+
+function DeployWebApplication {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the MsDeploy Package folder to be deployed.")]
+		[System.IO.DirectoryInfo]$packageFolder
+	)
+	Write-Output "Deploying Web Application $($packageFolder.Name)"
+	$settings = GetDeploymentSettings $packageFolder.Name
+	if ($settings -ne $null) {
+		if ($settings.DeploymentType -eq "Skip") {
+			Write-Output "Skipping Package $($packageFolder.Name)"
+			return
+		}
+		Write-Output "Deploying $($packageFolder.Name) to '$($settings.Server)/$($settings.SiteName)'...$($settings.ApplicationPath)..."
+		
+		$packageFile = "$($packageFolder)\$($settings.Name).zip"
+		$settingsFile = "$($packageFolder)\$($settings.Name).SetParameters.xml"
+		DeployDotNetWebApplication $packageFile $settingsFile $settings.Server $settings.SiteName $settings.ApplicationPath $settings.UserName $settings.Password	
+	}
+}
+
+function DeployWindowsServiceApplication {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the MsDeploy Package folder to be deployed.")]
+		[System.IO.DirectoryInfo]$packageFolder
+	)
+	Write-Output "Deploying Windows Service $($packageFolder.Name)"
+	$settings = GetDeploymentSettings $packageFolder.Name
+	if ($settings -ne $null) {
+		if ($settings.DeploymentType -eq "Skip") {
+			Write-Output "Skipping Package $($packageFolder.Name)"
+			return
+		}
+		Write-Output "Deploying $($packageFolder.Name) on $($settings.Server) as $($settings.ServiceName)..."
+		
+		$userName = $settings.UserName
+		$password = $settings.Password
+		
+		# Lookup credentials from KeePass if a Title is provided
+		if ([string]::IsNullOrEmpty($settings.KeePassTitle) -eq $false) {
+			$userName = QueryKeePass $settings.KeePassGroup $settings.KeePassTitle "UserName"
+			$password = QueryKeePass $settings.KeePassGroup $settings.KeePassTitle "Password"
+		}
+		
+		$packageFile = "$($packageFolder)\$($settings.Name).zip"
+
+		if ((IsNServiceBusService $packageFile) -eq $true) {
+			DeployNServiceBusWindowsService
+		}
+		else {
+			DeployDotNetWindowsService $packageFile $settings.Server $settings.ServiceName $userName $password
+		}
+	}
+}
+
+function GetDeploymentSettings {
+	Param (
+		[parameter(Mandatory=$true, Position=0, HelpMessage="The name of the application in the Deployment configuration file")]
+		[String]$packageFolderName
+	)
+	$deploymentConfigFile = "$($OutDir)\$($deploymentConfig)"
+	
+	if (Test-Path $deploymentConfigFile) {
+		[Xml]$xml = Get-Content $deploymentConfigFile
+		$app = $xml.SelectSingleNode("//Application[PackageFolderName='" + $packageFolderName + "']")
+		if ($app -eq $null) {
+			#throw ("Unable to find configuration for $packageFolderName")
+			Write-Output "Unable to find configuration for $($packageFolderName)"
+			return $null
+		}
+		else {
+			# Create custom object for deployment settings
+			#TODO: refactor to an array of custom objects
+			$server = ""
+			$appPath = ""
+			$user = ""
+			$password = ""
+			$sitename = ""
+			$serviceName = ""
+			$deploymentType = $app.Deployment.DeploymentType
+			$name = $app.Name
+			$keePassGroup = ""
+			$keePassTitle = ""
+					
+			if ($deploymentType -ne "Skip") {
+				$server = $app.Deployment.Servers.Server.Name
+				$appPath = $app.Deployment.Servers.Server.InstallationPath
+				$user = $app.Deployment.Servers.Server.UserName
+				$password = $app.Deployment.Servers.Server.Password
+				$sitename = $app.Deployment.Servers.Server.SiteName
+				$serviceName = $app.Deployment.Servers.Server.ServiceName
+				$keePassGroup = $app.Deployment.Servers.Server.KeePassGroup
+				$keePassTitle = $app.Deployment.Servers.Server.KeePassTitle
+			}
+			
+			$settings = New-Object PSObject
+			$settings | Add-Member NoteProperty -Name Name -Value $name
+			$settings | Add-Member NoteProperty -Name DeploymentType -Value $deploymentType
+			$settings | Add-Member NoteProperty -Name Server -Value $server
+			$settings | Add-Member NoteProperty -Name ApplicationPath -Value $appPath
+			$settings | Add-Member NoteProperty -Name UserName -Value $user
+			$settings | Add-Member NoteProperty -Name Password -Value $password
+			$settings | Add-Member NoteProperty -Name SiteName -Value $sitename
+			$settings | Add-Member NoteProperty -Name ServiceName -Value $serviceName
+			$settings | Add-Member NoteProperty -Name KeePassGroup -Value $keePassGroup
+			$settings | Add-Member NoteProperty -Name KeePassTitle -Value $keePassTitle
+
+			return $settings
+		}
+	}
+	else {
+		throw ("Deployment Configuration file not found at $deploymentConfigFile")
+	}
+
+}
+
